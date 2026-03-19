1b30af2ad66d56448d97293d75d639d0d6c45938
Feng <summitzf@gmail.com>
2026-03-18 21:47:32 -0400

    Version bumped to 0.2.2 to fix the missing pkg for WASM

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -53,8 +53,8 @@ describe('fips-crypto module', () => {
       expect(VERSION).toMatch(/^\d+\.\d+\.\d+$/);
     });
 
-    it('VERSION is 0.2.1', () => {
-      expect(VERSION).toBe('0.2.1');
+    it('VERSION is 0.2.2', () => {
+      expect(VERSION).toBe('0.2.2');
     });
   });
 
